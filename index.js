583358b14b2d6a6362da069a8f1ce3152caeb5a3
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2022-06-19 13:02:22 -0300

    Feat: add user sign-up post route

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ import cors from "cors";
 const server = express();
 
 server.use(cors());
+server.use(express.json());
 
+const users = [];
 const tweteroo = [];
 
-server.get("/rota", (req, res) => {
+server.post("/sign-up", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send("ok");
+});
+
+server.get("/tweets", (req, res) => {
   res.send("Oi");
 });
 
-server.post("/rota", (req, res) => {
+server.post("/tweets", (req, res) => {
   res.send("Oi");
 });
 
-server.listen(4000);
+server.listen(5000);
